1b1fe6fff85c216d64e22aa57d24fae53579a967
Fernando1215 <dafego13@gmail.com>
2017-01-12 10:58:33 -0500

    Add files via upload

---
+++ Prueba_Eclipse/src/com/pr1/GUI/Main.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.pr1.GUI;
+
+import javax.swing.JOptionPane;
+
+/**
+ * @author dgomez
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		JOptionPane.showMessageDialog(null, "Hello World", "HOLA MUNDO!!", JOptionPane.INFORMATION_MESSAGE);
+
+	}
+
+}
